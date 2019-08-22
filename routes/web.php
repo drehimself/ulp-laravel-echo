109248bcefedef63116971812d81d97030f4dbc1
Andre Madarang <andre_madarang@hotmail.com>
2019-08-22 01:54:42 -0400

    First commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Events\OrderShipped;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/fire', function () {
+    event(new OrderShipped);
+
+    return 'Event fired';
+});
